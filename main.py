79ddb98eb87315348a768f905f88a71ffff8b60f
halifk <aasadi.ai.work@gmail.com>
2021-08-29 21:43:22 -0400

    Restructures based on featureSelection update

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from Utilities import Utils
-from customDataLoaders import dataLoaderTabular
-from Classifier import BinaryClassifier,train,accuracy
-from display import displayLosses
+#%%
+from sklearn import feature_selection
+from Utilities.dataformater import DataFormater
+from FeatureEngineering_Selection.featureEngineering import featureCreation
+from FeatureEngineering_Selection.featureSelection import Selector
+import numpy as np
+#%%
 
-def main():
-    utilities = Utils()
-    test = BinaryClassifier("tab")
-    trainDataLoader,validationDataLoader,testDataLoader = dataLoaderTabular()
-    trainLoss,valLoss = train(test,trainDataLoader,validationDataLoader,epochs=1000)
-    print(accuracy(test,validationDataLoader))
-    displayLosses(trainLoss,valLoss)
+X_train,X_validation,X_test,y_train,y_validation,y_test = DataFormater().preProcessing(standardize=False)
+X_train = featureCreation(X_train)
 
-main()
+selector = Selector()
+formater = DataFormater()
+#standardize before PCA
+X_train = formater.standardize(X_train,useParams=False)
+X_validation = formater.standardize(X_validation,useParams=True)
+X_test = formater.standardize(X_test,useParams=True)
+#apply PCA
+X_train = selector.principleComponentAnalysis(X_train)
+X_validation = selector.principleComponentAnalysis(X_validation)
+X_test = selector.principleComponentAnalysis(X_test)
+#standardize again for fast convergence and no exploding gradients
+X_train = formater.standardize(X_train,useParams=False)
+X_validation = formater.standardize(X_validation,useParams=True)
+X_test = formater.standardize(X_test,useParams=True)
+
+
+# %%
