0e856696ddf6e4c9127143226ad84ced47a3ddae
halifk <aasadi.ai.work@gmail.com>
2021-08-29 21:44:26 -0400

    Refactors into new Selector Class and implements PCA

--- FeatureEngineering_Selection/featureSelection.py
+++ FeatureEngineering_Selection/featureSelection.py
@@ -11,10 +11,23 @@
 #Tuesday Morning: Presentation and GitHub readme.txt
 from sklearn import feature_selection
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.decomposition import PCA
+import pandas as pd
 
-def recursiveFeatureElimination(X,y,runs=10):
-    #run 10 times and take best n
-    featureSelector = feature_selection.RFE(estimator=DecisionTreeRegressor(),n_features_to_select=6)
-    featureSelector.fit(X,y)
-    bestFeatures = X.columns[featureSelector.support_].tolist()
-    return X[bestFeatures]
+class Selector():
+    def __init__(self):
+        self.featureSelector = feature_selection.RFE(estimator=DecisionTreeRegressor(),n_features_to_select=6)
+        self.pca = PCA(n_components=6)
+
+    def recursiveFeatureElimination(self,X,y,useParams=True,runs=10):
+        #run 10 times and take best n
+        if not useParams:
+            self.featureSelector.fit(X,y)
+        bestFeatures = X.columns[self.featureSelector.support_].tolist()
+        return X[bestFeatures]
+
+    def principleComponentAnalysis(self,X,useParams=True):
+        if not useParams:
+            self.pca.fit(X)
+        transformedDf = self.pca.transform(X)
+        return pd.DataFrame(data=transformedDf)
